4aefd5c64273884d00cfb5dcfbc09579c1e75553
picrossin <jadengoter@gmail.com>
2020-07-11 15:40:35 -0500

    Finished moving enemy.

---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> pathPoints = new List<GameObject>();
+
+    private void Update()
+    {
+        List<GameObject> pathPointsUpdated = new List<GameObject>();
+
+        foreach (Transform child in transform)
+        {
+            pathPointsUpdated.Add(child.gameObject);
+        }
+
+        if (pathPointsUpdated.Except(pathPoints).Count() > 0 ||
+            pathPoints.Except(pathPointsUpdated).Count() > 0)
+        {
+            pathPoints = pathPointsUpdated;
+        }
+    }
+
+    public List<GameObject> GetPathPoints()
+    {
+        return pathPoints;
+    }
+}
