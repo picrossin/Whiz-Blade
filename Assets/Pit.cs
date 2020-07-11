85e0d67c59128118022768c7e70d09e2596afcf4
zrandolph977 <zrandolph977@gmail.com>
2020-07-11 00:16:52 -0500

    Added pit
    basically just spikes right now. Have not added fly over or box filling functions as of yet.

---
+++ Assets/Pit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pit : MonoBehaviour
+{
+
+    [SerializeField] [Range(1, 10)] private int value = 6;
+
+    private GameObject player;
+
+    void Update()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+        else
+        {
+            if (Vector2.Distance(player.transform.position, transform.position) < 0.1f)
+            {
+                player.GetComponent<PlayerController>().IncreaseBloodLustCounter(value);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+}
