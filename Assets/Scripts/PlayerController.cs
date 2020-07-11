36a3a035c7503d563aeca5fb6cedb80df3f70793
picrossin <jadengoter@gmail.com>
2020-07-10 22:24:24 -0500

    Implemented player looking.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     // Movement
     [SerializeField] [Range(1.0f, 50.0f)] private float movementSmoothingAmount = 5.0f;
-    private bool canMove = true, isMoving = false;
+    private bool canMove = true, isMoving = false, flying = false;
     private Vector3 movement = Vector3.zero;
     private Vector3 smoothedPosition, targetPosition;
+    private Vector2 facing = Vector2.down;
 
     // Collisions
-    [SerializeField] private LayerMask wallLayerMask;
+    [SerializeField] private LayerMask wallLayerMask, enemyMask;
 
     private void Update()
     {
@@ -27,18 +28,23 @@ public class PlayerController : MonoBehaviour
             if (Input.GetButtonDown("Up") && !upHit)
             {
                 movement = new Vector3(0, 1);
+                facing = new Vector2(0, 1);
             }
             else if (Input.GetButtonDown("Down") && !downHit)
             {
                 movement = new Vector3(0, -1);
+                facing = new Vector2(0, -1);
             }
             else if (Input.GetButtonDown("Right") && !rightHit)
             {
                 movement = new Vector3(1, 0);
+                facing = new Vector2(1, 0);
+
             }
             else if (Input.GetButtonDown("Left") && !leftHit)
             {
                 movement = new Vector3(-1, 0);
+                facing = new Vector2(-1, 0);
             }
             else
             {
@@ -68,5 +74,26 @@ public class PlayerController : MonoBehaviour
                 isMoving = false;
             }
         }
+
+        // Look for enemies
+        RaycastHit2D enemyLook = Physics2D.Raycast(transform.position, facing, Mathf.Infinity, wallLayerMask | enemyMask);
+        if (enemyLook && enemyLook.transform.gameObject.layer == layerMaskToLayer(enemyMask))
+        {
+            flying = true;   
+        }
+    }
+
+    private int layerMaskToLayer(LayerMask layerMask)
+    {
+        int layerNumber = 0;
+        int layer = layerMask.value;
+
+        while (layer > 0)
+        {
+            layer >>= 1;
+            layerNumber++;
+        }
+
+        return layerNumber - 1;
     }
 }
