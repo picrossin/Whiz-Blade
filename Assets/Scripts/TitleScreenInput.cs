5057994f1fc531dc62f6b9bd9d17499371917473
picrossin <jadengoter@gmail.com>
2020-07-12 07:41:07 -0500

    Made temp title screen.

---
+++ Assets/Scripts/TitleScreenInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenInput : MonoBehaviour
+{
+    [SerializeField] private Animator fade;
+
+    private bool started = false;
+
+    private void Update()
+    {
+        if (!started)
+        {
+            if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2") || Input.GetButtonDown("Fire3") || Input.GetButtonDown("Submit"))
+            {
+                started = true;
+                fade.SetTrigger("SwipeIn");
+                StartCoroutine(WaitForTransition());
+            }
+        }
+    }
+
+    private IEnumerator WaitForTransition()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
